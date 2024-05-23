18ad9bc3ae888193de682c822219e9f943075a66
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-23 06:23:38 +0300

    changed error boundary style

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -4,7 +4,7 @@ function ErrorBoundary() {
   let error = useRouteError();
 
   return (
-    <div className="flex items-start px-72 mx-auto justify-center flex-col gap-2 h-screen">
+    <div className="flex items-start px-72 mx-auto justify-start mt-4 flex-col gap-2 h-screen">
       <h1 className="text-5xl">{error.status}</h1>
       <h2>{error.data}</h2>
       <p>{error.error.stack}</p>
