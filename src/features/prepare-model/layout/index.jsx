7056f4584a4bad9834fd0380ad38dd1e8757739a
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-01 23:41:23 +0300

    update: remove separator

--- src/features/prepare-model/layout/index.jsx
+++ src/features/prepare-model/layout/index.jsx
@@ -1,5 +1,4 @@
 import Heading from "../../../components/Heading";
-import { Separator } from "@/components/ui/separator";
 import PrepareModelForm from "../components/PrepareModelForm";
 
 function UploadModelLayout() {
@@ -7,8 +6,8 @@ function UploadModelLayout() {
     <div className="grid grid-cols-12 gap-4">
       <div className="col-start-4 col-end-10">
         <Heading as="h1">Prepare Model</Heading>
-        <Separator className="mt-3" />
-        <div className="mt-6">
+
+        <div className="mt-4">
           <PrepareModelForm />
         </div>
       </div>
