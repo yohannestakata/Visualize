b0a751a9d160567b45996d6088fbf55b905c41cb
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 07:07:05 +0300

    Changed serve url

--- src/data/globals.js
+++ src/data/globals.js
@@ -1,1 +1,3 @@
-export const SERVER_URL = "http://localhost:3000";
+export const SERVER_URL =
+  "https://visualize-hvfrepmbh-yohannes-takatas-projects.vercel.app/3000";
+// export const SERVER_URL = "http://localhost:3000";
