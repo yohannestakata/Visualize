46f403fe1263b3ff050a282414574b14fad2b7fd
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 12:42:41 +0300

    Changed backend host

--- src/data/globals.js
+++ src/data/globals.js
@@ -4,4 +4,4 @@ console.log(import.meta.env.DEV);
 console.log(import.meta.env.PROD);
 
 if (import.meta.env.DEV) SERVER_URL = "http://localhost:3000";
-if (import.meta.env.PROD) SERVER_URL = "https://visualize-api.vercel.app";
+if (import.meta.env.PROD) SERVER_URL = "https://visualize-api.onrender.com";
