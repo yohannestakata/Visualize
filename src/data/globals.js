3c9582d0e159657fdad70e3235cee4b523c824a6
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 07:33:15 +0300

    Defined prod and env server urls

--- src/data/globals.js
+++ src/data/globals.js
@@ -1,3 +1,9 @@
-export const SERVER_URL =
-  "https://visualize-hvfrepmbh-yohannes-takatas-projects.vercel.app/3000";
-// export const SERVER_URL = "http://localhost:3000";
+export let SERVER_URL = "";
+
+console.log(import.meta.env.DEV);
+console.log(import.meta.env.PROD);
+
+if (import.meta.env.DEV)
+  SERVER_URL =
+    "https://visualize-hvfrepmbh-yohannes-takatas-projects.vercel.app/3000";
+if (import.meta.env.PROD) SERVER_URL = "http://localhost:3000";
