172f6a878568ef19300ef056692b8010126131a6
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 07:41:04 +0300

    Switched dev and prod server urls

--- src/data/globals.js
+++ src/data/globals.js
@@ -3,7 +3,7 @@ export let SERVER_URL = "";
 console.log(import.meta.env.DEV);
 console.log(import.meta.env.PROD);
 
-if (import.meta.env.DEV)
+if (import.meta.env.PROD)
   SERVER_URL =
     "https://visualize-hvfrepmbh-yohannes-takatas-projects.vercel.app/3000";
-if (import.meta.env.PROD) SERVER_URL = "http://localhost:3000";
+if (import.meta.env.DEV) SERVER_URL = "http://localhost:3000";
