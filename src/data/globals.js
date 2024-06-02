8c68f3d258064dd52360fed869be99b972b7ede7
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-02 09:37:01 +0300

    Change prod server url

--- src/data/globals.js
+++ src/data/globals.js
@@ -3,5 +3,5 @@ export let SERVER_URL = "";
 console.log(import.meta.env.DEV);
 console.log(import.meta.env.PROD);
 
-if (import.meta.env.PROD) SERVER_URL = "https://visualize-api.vercel.app:3000";
 if (import.meta.env.DEV) SERVER_URL = "http://localhost:3000";
+if (import.meta.env.PROD) SERVER_URL = "https://visualize-api.vercel.app";
