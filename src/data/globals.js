f1b9011721353b17752b242d1090ca71141dfe7c
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-03 14:01:57 +0300

    Removed console.log()

--- src/data/globals.js
+++ src/data/globals.js
@@ -1,7 +1,4 @@
 export let SERVER_URL = "";
 
-console.log(import.meta.env.DEV);
-console.log(import.meta.env.PROD);
-
 if (import.meta.env.DEV) SERVER_URL = "http://localhost:3000";
 if (import.meta.env.PROD) SERVER_URL = "https://visualize-api.onrender.com";
