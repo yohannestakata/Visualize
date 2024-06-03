8d5045ad8a0fcffde813297bb90ae73e85215c5d
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-03 14:04:02 +0300

    Handle verification errors

--- src/features/auth/hooks/useVerifyUser.js
+++ src/features/auth/hooks/useVerifyUser.js
@@ -1,11 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { verifyUser } from "../services";
+import { useToast } from "@/components/ui/use-toast";
+import { useNavigate } from "react-router-dom";
 
 function useVerifyUser() {
-  const { data, isLoading } = useQuery({
+  const navigate = useNavigate();
+  const { toast } = useToast();
+  const { data, isLoading, error } = useQuery({
     queryFn: verifyUser,
     queryKey: ["user-verify"],
   });
+
+  if (error) {
+    toast({
+      title: "Error",
+      description: "Session expired. Please login again.",
+      variant: "destructive",
+    });
+    navigate("/auth/login");
+  }
 
   return { data: data?.data, isLoading };
 }
