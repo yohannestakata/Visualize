f7de3ca87ad80b81af7fa94c58530b481f8d1f2c
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-04 03:23:07 +0300

    Handle thumbnail updates

--- src/services/modelApi.js
+++ src/services/modelApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { SERVER_URL } from "../data/globals";
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getModel(id) {
   const { data } = await axios({
@@ -20,7 +20,16 @@ export async function getModel(id) {
   return { ...modelData, model };
 }
 
-export function updateModel(id, data) {
+export async function updateModel(id, data) {
+  if (data.thumbnail) {
+    const { data: thumbnailData, error: thumbnailError } =
+      await supabase.storage
+        .from("thumbnails")
+        .upload(`${Date.now()}-${data.thumbnail.name}`, data.thumbnail);
+    data.thumbnailUrl = `${supabaseUrl}/storage/v1/object/public/${thumbnailData.fullPath}`;
+    if (thumbnailError) throw new Error("Error uploading thumbnail");
+  }
+
   return axios({
     url: `${SERVER_URL}/models/${id}`,
     data,
