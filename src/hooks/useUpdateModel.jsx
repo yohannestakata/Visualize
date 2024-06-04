52db5b2afbd15a3259e469ae16d1ca5d5f0987e6
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-04 03:23:37 +0300

    Invalidate queries on model update

--- src/hooks/useUpdateModel.jsx
+++ src/hooks/useUpdateModel.jsx
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateModel } from "../services/modelApi";
 import { useToast } from "@/components/ui/use-toast";
 
 function useUpdateModel(id) {
   const { toast } = useToast();
+  const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationFn: (data) => updateModel(id, data),
     onSuccess: () => {
+      queryClient.invalidateQueries(["model", "models"]);
       toast({
         title: "Success!",
         description: "Your changes are saved.",
