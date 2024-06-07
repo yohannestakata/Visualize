8ea47c6adab5131c4bd1c2660ca1569a5380c189
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-07 08:59:18 +0300

    Check for jwt token before verifying user

--- src/features/auth/components/ProtectRoute.jsx
+++ src/features/auth/components/ProtectRoute.jsx
@@ -3,15 +3,20 @@ import useVerifyUser from "../hooks/useVerifyUser";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import useUser from "../../../hooks/useUser";
+import { useQueryClient } from "@tanstack/react-query";
 
 axios.defaults.withCredentials = true;
 
 function ProtectRoutes({ children }) {
   const navigate = useNavigate();
   const { dispatch } = useUser();
+  const token = localStorage.getItem("jwt");
+
   const { data, isLoading } = useVerifyUser();
 
   const user = data?.user;
+
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     if (isLoading) return;
@@ -22,7 +27,13 @@ function ProtectRoutes({ children }) {
     if (user?.role === "Student") navigate("/learn");
     if (user?.role === "Admin") navigate("/admin");
     if (user?.role === "Teacher") navigate("/teacher/classrooms");
-  }, [user, isLoading, navigate, dispatch]);
+
+    return () => {
+      queryClient.invalidateQueries({ queryKey: ["user-verify"] });
+    };
+  }, [user, isLoading, navigate, dispatch, queryClient, token]);
+
+  if (isLoading) return <div>Loading...</div>;
 
   return <div>{children}</div>;
 }
