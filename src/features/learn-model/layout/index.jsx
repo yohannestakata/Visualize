cbd7998b1b926f513a7e187bf8907ed5a3c0ffe1
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-09 23:09:55 +0300

    make interface resizable

--- src/features/learn-model/layout/index.jsx
+++ src/features/learn-model/layout/index.jsx
@@ -1,5 +1,53 @@
+import { useSearchParams } from "react-router-dom";
+import useGetModel from "../../../hooks/useGetModel";
+import Model from "../../../components/Model";
+import { Suspense } from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import Heading from "../../../components/Heading";
+import { Loader2 } from "lucide-react";
+
 function LearnModelLayout() {
-  return <div>Learn model layout</div>;
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("modelId");
+  const { data: model, isLoading: isModelLoading } = useGetModel(id);
+  console.log(model);
+  return (
+    <div className="mt-6">
+      <Heading as={"h1"}>{model?.modelTitle}</Heading>
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="mt-4 min-h-96 rounded-lg border"
+      >
+        <ResizablePanel defaultSize={66}>
+          <div className="aspect-photo bg-secondary">
+            <Suspense fallback={<Loader2 className="animate-spin" />}>
+              {!isModelLoading && <Model modelUrl={model?.modelUrl} />}
+            </Suspense>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={33}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={50}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Two</span>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={50}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Three</span>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 }
 
 export default LearnModelLayout;
