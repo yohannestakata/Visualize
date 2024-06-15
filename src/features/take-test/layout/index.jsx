4ae559b5765f9ef8c75c16bbd947dca27e928e25
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-15 20:36:28 +0300

    Added input for submitting part name

--- src/features/take-test/layout/index.jsx
+++ src/features/take-test/layout/index.jsx
@@ -3,7 +3,7 @@ import useGetModel from "../../../hooks/useGetModel";
 import Model from "../../../components/Model";
 import { Suspense, useEffect, useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
-
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -84,41 +84,57 @@ function TakeTestLayout() {
         className="mt-5 h-full min-h-96 flex-1 rounded-lg "
       >
         <ResizablePanel defaultSize={50}>
-          <div className="aspect-square h-full bg-secondary">
-            <Suspense fallback={<Loader2 className="animate-spin" />}>
-              {!isModelLoading && (
-                <Model
-                  modelUrl={model?.modelUrl}
-                  clickedMesh={clickedMesh}
-                  showHelper={false}
-                  setMeshes={setMeshes}
-                />
-              )}
-            </Suspense>
+          <div className="pr-3">
+            <div className="aspect-square h-full rounded-lg bg-muted">
+              <Suspense fallback={<Loader2 className="animate-spin" />}>
+                {!isModelLoading && (
+                  <Model
+                    modelUrl={model?.modelUrl}
+                    clickedMesh={clickedMesh}
+                    showHelper={false}
+                    setMeshes={setMeshes}
+                  />
+                )}
+              </Suspense>
+            </div>
           </div>
         </ResizablePanel>
         <ResizableHandle />
 
         <ResizablePanel defaultSize={50}>
-          <div className="p-6 pt-0">
-            <div className="flex items-center justify-between">
-              <Heading as={"h2"} className={"flex items-center gap-2"}>
-                <ListChecks />
-                {`Your answers (0/${meshes?.length})`}
-              </Heading>
-              <span>
-                {getTimeLeft(timeLeft) !== "00:00" ? (
-                  getTimeLeft(timeLeft)
-                ) : (
-                  <span className="flex items-center font-semibold text-destructive">
-                    <Timer className="mr-2 h-4 w-4 " /> Time&apos;s up!
-                  </span>
-                )}
-              </span>
+          <div className="flex h-full flex-col pl-3">
+            <div className="flex h-full w-full flex-1 flex-col gap-4 rounded-lg border p-6">
+              <div className="flex items-center justify-between">
+                <Heading as={"h2"} className={"flex items-center gap-2"}>
+                  <ListChecks />
+                  {`Your answers (0/${meshes?.length})`}
+                </Heading>
+                <span>
+                  {getTimeLeft(timeLeft) !== "00:00" ? (
+                    getTimeLeft(timeLeft)
+                  ) : (
+                    <span className="flex items-center font-semibold text-destructive">
+                      <Timer className="mr-2 h-4 w-4 " /> Time&apos;s up!
+                    </span>
+                  )}
+                </span>
+              </div>
+              <ScrollArea className="flex-1">
+                <p className="break-all">e</p>
+              </ScrollArea>
+              <div className="flex gap-2">
+                <Input
+                  placeholder="Enter part name"
+                  disabled={!startedTest || getTimeLeft(timeLeft) === "00:00"}
+                />
+                <Button
+                  vairant="secondary"
+                  disabled={!startedTest || getTimeLeft(timeLeft) === "00:00"}
+                >
+                  Submit
+                </Button>
+              </div>
             </div>
-            <ScrollArea className="h-full w-full ">
-              <p className="mt-2 break-all pb-2">e</p>
-            </ScrollArea>
           </div>
         </ResizablePanel>
       </ResizablePanelGroup>
@@ -145,9 +161,17 @@ function TakeTestLayout() {
               Time given to complete the test is{" "}
               <span className="font-semibold">{getTimeLeft(timeLeft)}</span>.
             </li>
+            <li>
+              Closing the page mid test or being unable to finish the test on
+              time dismisses your progress.
+            </li>
+            <li>
+              Don&apos;t forget to click the finish button that appears once
+              done.
+            </li>
           </ul>
           <DialogFooter>
-            <span>Good Luck!</span>
+            <span>Good Luck! 😉</span>
           </DialogFooter>
         </DialogContent>
       </Dialog>
