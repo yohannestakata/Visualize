c3ad835dec1d9c06779560911582125a98f3ff42
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-15 13:06:36 +0300

    take test button and layout change

--- src/features/learn-model/layout/index.jsx
+++ src/features/learn-model/layout/index.jsx
@@ -1,8 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
 import useGetModel from "../../../hooks/useGetModel";
 import Model from "../../../components/Model";
 import { Suspense, useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Textarea } from "@/components/ui/textarea";
 
 import {
   ResizableHandle,
@@ -10,7 +11,8 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import Heading from "../../../components/Heading";
-import { Loader2, Volume2 } from "lucide-react";
+import { Loader2, PencilLine, Volume2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 function LearnModelLayout() {
   const [searchParams] = useSearchParams();
@@ -38,19 +40,20 @@ function LearnModelLayout() {
   }
 
   return (
-    <div className="mt-6 ">
-      <Heading as={"h1"}>{model?.modelTitle}</Heading>
+    <div className="mt-6 flex flex-1 flex-col">
+      <div className="flex items-center justify-between">
+        <Heading as={"h1"}>Chapter: {model?.modelTitle}</Heading>
+        <NavLink to={`/learn/take-test?modelId=${model?._id}`}>
+          <Button className="flex items-center gap-2">
+            <PencilLine className=" h-4 w-4" /> Take test
+          </Button>
+        </NavLink>
+      </div>
       <ResizablePanelGroup
         direction="horizontal"
-        className="mt-4 min-h-96 rounded-lg border"
+        className="mt-5 h-full min-h-96 flex-1 rounded-lg "
       >
-        <ResizablePanel defaultSize={30}>
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Three</span>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={40}>
+        <ResizablePanel defaultSize={50}>
           <div className="aspect-square h-full bg-secondary">
             <Suspense fallback={<Loader2 className="animate-spin" />}>
               {!isModelLoading && (
@@ -65,25 +68,46 @@ function LearnModelLayout() {
           </div>
         </ResizablePanel>
         <ResizableHandle />
-        <ResizablePanel defaultSize={30}>
-          <div className="h-full p-6">
-            <Heading as={"h2"} className={"flex items-center gap-2"}>
-              <span>{clickedMesh.name || "Nothing selected"}</span>
-              <Volume2
-                onMouseDown={() => speak(clickedMesh.name)}
-                className="cursor-pointer"
-              />
-            </Heading>
-            <ScrollArea className="h-full w-full ">
-              <p className="mt-2 break-all pb-2">
-                {
-                  model?.definitions.find(
-                    (def) => def.title === clickedMesh.name,
-                  )?.definition
-                }
-              </p>
-            </ScrollArea>
-          </div>
+        <ResizablePanel defaultSize={50}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={50}>
+              <div className="p-6 pt-0">
+                <Heading as={"h2"} className={"flex items-center gap-2"}>
+                  <span>{clickedMesh.name || "Nothing selected"}</span>
+                  <Volume2
+                    onMouseDown={() => speak(clickedMesh.name)}
+                    className="cursor-pointer"
+                  />
+                </Heading>
+                <ScrollArea className="h-full w-full ">
+                  <p className="mt-2 break-all pb-2">
+                    {
+                      model?.definitions.find(
+                        (def) => def.title === clickedMesh.name,
+                      )?.definition
+                    }
+                  </p>
+                </ScrollArea>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel>
+              <div className="flex h-full flex-col gap-3 p-6 pb-0">
+                <Heading as="h2">Assistant</Heading>
+                <ScrollArea className="h-full flex-1">
+                  <p></p>
+                </ScrollArea>
+                <div className="flex gap-2">
+                  <Textarea
+                    placeholder="Message assistant"
+                    rows={1}
+                    className="min-h-0 items-end justify-end"
+                  />
+                  <Button variant="secondary">Send</Button>
+                </div>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
