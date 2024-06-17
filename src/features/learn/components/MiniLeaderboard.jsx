9b76f4ee012ea0c99eafa66f48822b3c7fdbb64e
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-17 20:30:40 +0300

    redesigned leaderboard

--- src/features/learn/components/MiniLeaderboard.jsx
+++ src/features/learn/components/MiniLeaderboard.jsx
@@ -1,24 +1,33 @@
-import { Star } from "lucide-react";
-
 function MiniLeaderboard() {
   return (
-    <div className="">
-      <span className="font-semibold text-xl">Leaderboards</span>
-      <div className="flex flex-col gap-2 mt-2">
-        <span className="flex items-center gap-2">
-          <Star className="w-4 h-4" /> Yohannes Takata
-        </span>
-        <span className="flex items-center gap-2">
-          <Star className="w-4 h-4" /> Yohannes Takata
-        </span>
-        <span className="flex items-center gap-2">
-          <Star className="w-4 h-4" /> Yohannes Takata
-        </span>
-        <span className="flex items-center gap-2">
-          <span className="font-semibold">#32</span>
-          <span className="font-semibold">Yohannes Takata</span>
-        </span>
-      </div>
+    <div className="rounded-lg border bg-card p-6">
+      <span className="text-xl font-semibold">Leaderboards</span>
+      <table className="mt-6 ">
+        <tbody>
+          <tr>
+            <td className="py-1 pr-2 text-right text-lg">#1</td>
+            <td className="px-2 py-1 text-lg ">Yohannes Takata</td>
+            <td className="px-2 py-1 text-right  text-lg">30000 points</td>
+          </tr>
+          <tr>
+            <td className="py-1 pr-2 text-right  text-lg">#2</td>
+            <td className="px-2 py-1 text-lg ">John Doe</td>
+            <td className="px-2 py-1 text-right  text-lg">25600 points</td>
+          </tr>
+          <tr>
+            <td className="py-1 pr-2 text-right  text-lg">#3</td>
+            <td className="px-2 py-1 text-lg ">Jane Doe</td>
+            <td className="px-2 py-1 text-right  text-lg">25200 points</td>
+          </tr>
+          <tr className=" text-primary">
+            <td className="py-1 pr-2 text-right text-lg font-semibold">#7</td>
+            <td className="px-2 py-1 text-lg font-semibold">You</td>
+            <td className="px-2 py-1 text-right text-lg font-semibold">
+              17000 points
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
