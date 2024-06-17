2ba1916ea30c6f81013ea1a1371355faec8a8e5f
Yohannes Takata <yohannestakata1@gmail.com>
2024-06-17 20:38:13 +0300

    wider cell padding

--- src/features/learn/components/MiniLeaderboard.jsx
+++ src/features/learn/components/MiniLeaderboard.jsx
@@ -5,24 +5,24 @@ function MiniLeaderboard() {
       <table className="mt-6 ">
         <tbody>
           <tr>
-            <td className="py-1 pr-2 text-right text-lg">#1</td>
-            <td className="px-2 py-1 text-lg ">Yohannes Takata</td>
-            <td className="px-2 py-1 text-right  text-lg">30000 points</td>
+            <td className="py-1 pr-3 text-right text-lg">#1</td>
+            <td className="px-3 py-1 text-lg ">Yohannes Takata</td>
+            <td className="py-1 pl-3 text-right  text-lg">30000 points</td>
           </tr>
           <tr>
-            <td className="py-1 pr-2 text-right  text-lg">#2</td>
-            <td className="px-2 py-1 text-lg ">John Doe</td>
-            <td className="px-2 py-1 text-right  text-lg">25600 points</td>
+            <td className="py-1 pr-3 text-right  text-lg">#2</td>
+            <td className="px-3 py-1 text-lg ">John Doe</td>
+            <td className="py-1 pl-3 text-right  text-lg">25600 points</td>
           </tr>
           <tr>
-            <td className="py-1 pr-2 text-right  text-lg">#3</td>
-            <td className="px-2 py-1 text-lg ">Jane Doe</td>
-            <td className="px-2 py-1 text-right  text-lg">25200 points</td>
+            <td className="py-1 pr-3 text-right  text-lg">#3</td>
+            <td className="px-3 py-1 text-lg ">Jane Doe</td>
+            <td className="py-1 pl-3 text-right  text-lg">25200 points</td>
           </tr>
           <tr className=" text-primary">
-            <td className="py-1 pr-2 text-right text-lg font-semibold">#7</td>
-            <td className="px-2 py-1 text-lg font-semibold">You</td>
-            <td className="px-2 py-1 text-right text-lg font-semibold">
+            <td className="py-1 pr-3 text-right text-lg font-semibold">#7</td>
+            <td className="px-3 py-1 text-lg font-semibold">You</td>
+            <td className="py-1 pl-3 text-right text-lg font-semibold">
               17000 points
             </td>
           </tr>
