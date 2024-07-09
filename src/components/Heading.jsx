673666c796d4b88218ec1454dddd9387eeecf4d0
Yohannes Takata <yohannestakata1@gmail.com>
2024-07-09 19:15:14 +0300

    default heading h1

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,11 +1,13 @@
-function Heading({ as, children, className }) {
+function Heading({ as = "h1", children, className }) {
   switch (as) {
     case "h1":
       return (
         <h1 className={`text-3xl font-semibold ${className}`}>{children}</h1>
       );
     case "h2":
-      return <h2 className={`text-xl ${className}`}>{children}</h2>;
+      return (
+        <h2 className={`text-xl font-semibold ${className}`}>{children}</h2>
+      );
     case "h3":
       return <h3 className={`font-semibold ${className}`}>{children}</h3>;
     default:
